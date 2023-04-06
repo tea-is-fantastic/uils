c8fa523d807cc577291e608be66ea28cd2a9a081
Asfandyar Sheikh <andy@replyqa.com>
2023-04-07 04:44:35 +0500

    woohoo

--- src/paths.js
+++ src/paths.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-const src = path.join(process.env.INIT_CWD, process.env.TISF_SRC);
+const src = path.join(process.env.INIT_CWD, process.env.TISF_SRC || 'scripts');
 const dataPath = path.join(process.env.INIT_CWD, 'data');
 const assetsPath = path.join(process.env.INIT_CWD, 'assets');
 const envPath = path.join(process.env.INIT_CWD, 'env');
